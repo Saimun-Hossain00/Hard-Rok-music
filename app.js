50d99f29a86eb6ac55f12143921814786a21f440
Saimun Hossain <saimunhossaiemon@gmail.com>
2021-02-10 11:16:41 +0600

    all work done

---
+++ app.js
@@ -0,0 +1,42 @@
+const searchSongs = async() => {
+    const searchText = document.getElementById('search-field').value;
+    const url = (`https://api.lyrics.ovh/suggest/${searchText}`)
+    //load
+      const res = await fetch(url);
+      const data = await res.json();                    
+      displaySongs(data.data);
+}
+
+const displaySongs = songs => {
+    const songContainer = document.getElementById('song-container');
+    songContainer.innerHTML = '';
+
+    songs.forEach(song => {
+        const songDiv = document.createElement('div');
+        songDiv.className = 'single-result row align-items-center my-3 p-3';
+        songDiv.innerHTML = `
+        <div class="col-md-9">
+        <h3 class="lyrics-name">${song.title}</h3>
+        <p class="author lead">Album by <span>${song.artist.name}</span</span></p>
+        <audio controls>
+      <source src="${song.preview}" type="audio/mpeg">
+      </audio>
+    </div>
+    <div class="col-md-3 text-md-right text-center">
+        <button onclick ="getLyric('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
+    </div>`;
+        songContainer.appendChild(songDiv);
+    });
+}
+
+const getLyric = async (artist,title) => {
+    const url = ` https://api.lyrics.ovh/v1/${artist}/${title}`;
+     const res = await fetch(url);
+    const data = await res.json();
+    displayLyrics(data.lyrics);
+}
+
+const displayLyrics = lyrics => {
+    const lyricsDiv = document.getElementById('song.lyrics');
+    lyricsDiv.innerText = lyrics;
+}
